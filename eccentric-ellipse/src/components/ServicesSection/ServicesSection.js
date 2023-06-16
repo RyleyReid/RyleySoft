4ba8c87f0d0df80649768d971a6d2a4ffeea29ee
RyleyReid <ryleycreid@gmail.com>
2023-06-16 14:40:22 -0400

    spinner tech info

--- eccentric-ellipse/src/components/ServicesSection/ServicesSection.js
+++ eccentric-ellipse/src/components/ServicesSection/ServicesSection.js
@@ -1,22 +1,35 @@
 var angle = 0;
-var index = 0;  // Add an index to keep track of the current image
+var index = 0;  
 
 const gallerySpin = (sign) => {
     const spinner = document.getElementById('spinner');
-    const images = spinner.getElementsByTagName('img');  // Get the list of images
+    const images = spinner.getElementsByTagName('img');
 
     if (!sign) {
         angle = angle + 45;
-        index = (index + 1) % images.length;  // Increment the index, loop back to 0 if it exceeds the number of images
+        index = (index + 1) % images.length;
     } else {
         angle = angle - 45;
-        index = (index - 1 + images.length) % images.length;  // Decrement the index, loop back to the last image if it's less than 0
+        index = (index - 1 + images.length) % images.length;
     }
 
     spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
 
-    const description = document.getElementById('description');  // Get the description element
-    description.innerText = images[index].alt;  // Set the description text to the alt text of the current image
+    const description = document.getElementById('description');
+    description.innerText = images[index].alt;
+    styleInActive(images);
+    styleActive(images[index]);
+
+}
+
+
+const styleActive = (imageObject) => {
+    imageObject.style.opacity = 1;
+}
+const styleInActive = (images) => {
+    Array.from(images).forEach(image => {
+        image.style.opacity = 0.5;
+    });
 }
 
 const leftButton = document.getElementById('leftButton');
