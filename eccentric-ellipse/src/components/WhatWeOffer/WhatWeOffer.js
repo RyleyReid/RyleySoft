9707042296872177c177db205fc64e707c13946f
Ryley Reid <ryleycreid@gmail.com>
2023-07-02 22:53:06 -0400

    colour change

--- eccentric-ellipse/src/components/WhatWeOffer/WhatWeOffer.js
+++ eccentric-ellipse/src/components/WhatWeOffer/WhatWeOffer.js
@@ -1,5 +1,5 @@
 const sparkle = document.querySelector('#sparkle');
-
+sparkle.remove()
 document.addEventListener('mousemove', e => {
     sparkle.style.left = e.pageX - 25 + 'px';
     sparkle.style.top = e.pageY - 25 + 'px';
